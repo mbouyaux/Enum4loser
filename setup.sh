3149e2482062219dd8755006e6d2a118b568d7c6
Romain <74216407+spawnzii@users.noreply.github.com>
2022-01-04 22:34:03 +0100

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# requirements installation
+
+pip install -r requirements.txt
+git clone https://github.com/aboul3la/Sublist3r.git
+cd Sublist3r/ ;pip install -r requirements.txt;cp -r subbrute /usr/local/bin/ ; cp sublist3r.py /usr/local/bin/
+rm -rf Sublist3r/
+
+sudo apt install golang-go
+sudo apt install gccgo-go
+go get github.com/tomnomnom/waybackurls
+cp $HOME/go/bin/waybackurls /usr/local/bin/
