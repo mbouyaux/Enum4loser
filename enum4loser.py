f5003f43ccf119c66d74b876a9e90b488f90ba5e
Romain <74216407+spawnzii@users.noreply.github.com>
2022-01-06 12:51:05 +0100

    Update enum4loser.py

--- enum4loser.py
+++ enum4loser.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-# By SpawnZii Version 0.1
+# By SpawnZii Version 0.3
 
 from posix import NGROUPS_MAX
 import sys,getopt
@@ -8,8 +8,11 @@ import os
 import pyfiglet
 import threading
 import getpass
+import requests
+import aiohttp
+import asyncio
 
-open_redirect_parameter = ['=http','=https','=www']
+open_redirect_parameter = ['=http','=https','=www','=/','=//']
 current_user = getpass.getuser()
 hunt_folder = "bugbounty"
 target_folder = "target"
@@ -75,27 +78,56 @@ def open_redirect(name_folder):
                 for p in open_redirect_parameter:
                     if p in url :
                         file_open_path = '/home/{}/Desktop/{}/{}/{}/redirect'.format(current_user,hunt_folder,target_folder,name_folder)
-                        if url not in known_url:
+                        if url.split('=')[0] not in known_url:
                             f = open(file_open_path, "a")
                             f.write(url)
                             f.close()
-                            known_url.append(url)
+                            known_url.append(url.split('=')[0])
+async def fetch(URL):
+    async with aiohttp.ClientSession(trust_env=True) as session:
+        try:
+            async with session.get(URL,allow_redirects=True) as response:
+                rep = response.url 
+                repcheck = response.real_url
+                in_url = str(URL)
+                if in_url not in str(rep) and in_url not in str(repcheck):
+                    print("\033[91m[$$$] Open redirect found at : {}\033[0m".format(URL))
+        except aiohttp.ClientConnectionError as e:
+            pass
+
+async def start(url):
+    await fetch(url)
+
+def trigger_open_redirect(name_folder,open_redirect_parameter,attacker_srv):
+    path = "/home/{}/Desktop/{}/{}/{}/".format(current_user,hunt_folder,target_folder,name_folder)
+    file_of_vuln_urls = open(path+"redirect")
+
+    for url in file_of_vuln_urls:
+        for i in open_redirect_parameter:
+            if url.count(i) > 0:
+                url = url.split("=")
+                url_done = url[0] + "="+attacker_srv+"/?vuln_url="+url[0]
+                loop = asyncio.new_event_loop()
+                loop.run_until_complete(start(url_done))
+
 
 def main(argv):
     banner_one = pyfiglet.figlet_format("Enum4loser")
     print("\033[91m"+banner_one[0:232]+"\033[0m"+"\033[92m"+banner_one[232:300]+"\033[0m")
-    print('Version 0.1')
+    print('Version 0.3')
     print('By SpawnZii made with \033[91m<3\033[0m \n')
     target = ''
     name_folder = ''
+    attacker_srv = ''
     try:
-        opts, args = getopt.getopt(argv,"hd:f:g",["domain="])
+        opts, args = getopt.getopt(argv,"hd:f:s:",["domain="])
     except getopt.GetoptError:
       print("""Usage: python3 enum4loser.py -d target.com -f name_of_folder
 
         -d    Domain name.
         -f    Name of the folder 
-Quick start: python3 enum4loser.py -d yeswehack.com -f ywhack""")
+        -s    Server of attaker.
+Quick start: python3 enum4loser.py -d yeswehack.com -f ywhack -s https://attacker.com""")
       sys.exit(2)
     for opt, arg in opts:
         if opt == '-h':
@@ -109,9 +141,12 @@ Quick start: python3 enum4loser.py -d yeswehack.com -f ywhack""")
             target = arg
         elif opt in ("-f", "--folder"):
             name_folder = arg
-            creat_folder(name_folder)
-            subdomain_enum(target,name_folder)
-            scrap_url(name_folder)
+            #creat_folder(name_folder)
+            #subdomain_enum(target,name_folder)
+            #scrap_url(name_folder)
+        elif opt in ("-s","--server"):
+            attacker_srv = arg
+            trigger_open_redirect(name_folder,open_redirect_parameter,attacker_srv)
 
 if __name__ == "__main__":
    main(sys.argv[1:])
