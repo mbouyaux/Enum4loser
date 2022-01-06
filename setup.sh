d7c3fe7007aa96675732e18f78f4cd375132cb90
Romain <74216407+spawnzii@users.noreply.github.com>
2022-01-06 13:02:25 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # requirements installation
 
+chmod +x enum4loser.py
 pip install -r requirements.txt
 git clone https://github.com/aboul3la/Sublist3r.git
 cd Sublist3r/ ;pip install -r requirements.txt;cp -r subbrute /usr/local/bin/ ; cp sublist3r.py /usr/local/bin/ ; cd ../ ; rm -rf Sublist3r/
