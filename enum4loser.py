2db11f2f0d1462be645288af989068a64baafeb4
Romain <74216407+spawnzii@users.noreply.github.com>
2022-01-19 00:05:07 +0100

    change cli

--- enum4loser.py
+++ enum4loser.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 # By SpawnZii Version 0.4
 
+from email.policy import default
+from fileinput import filename
+from http import server
+import click
 import sys,getopt
 import pyfiglet
 from pathlib import Path
@@ -9,45 +13,37 @@ from functions.subenum import *
 from functions.urlwayback import *
 
 home_path = str(Path.home())
-hunt_folder = "bugbounty" # path for outpout folder /home/user/Desktop/bugbounty/target/
-target_folder = "target"  # you can change the value of this two variables
+urlsorting_parameter = ['=http','=https','=www','=/','=//']
+hunt_folder = "bugbounty"
+target_folder = "target"
+@click.command()
 
+@click.option('--domain','-d',help='target domain name')
+@click.option('--filename','-f',help='name of the output folder store at ~/Desktop/bugbouty/target/here')
+@click.option('--server','-s',default='https://www.google.com',help='You can add your web server to see the logs in live')
+@click.option('--nosub',default="False",help='This options enumerate juste the main domain and not the subdomain')
 
-def main(argv):
+def main(domain,filename,server,nosub):
     banner_one = pyfiglet.figlet_format("Enum4loser")
     print("\033[91m"+banner_one[0:232]+"\033[0m"+"\033[92m"+banner_one[232:300]+"\033[0m")
-    print('Version 0.4')
+    print('Version 0.5')
     print('By SpawnZii made with \033[91m<3\033[0m \n')
-    target = ''
-    name_folder = ''
-    attacker_srv = ''
-    try:
-        opts, args = getopt.getopt(argv,"hd:f:s:",["domain="])
-    except getopt.GetoptError:
-      print("""Usage: python3 enum4loser.py -d target.com -f name_of_folder
+    name_folder = domain
+    target = filename
+    attacker_srv = server
+    nosub = str(nosub.lower())
+    if nosub == "true":
+        print("Under dev")
 
-        -d    Domain name.
-        -f    Name of the folder 
-        -s    Server of attaker.
-Quick start: python3 enum4loser.py -d yeswehack.com -f ywhack -s https://attacker.com""")
-      sys.exit(2)
-    for opt, arg in opts:
-        if opt == '-h':
-            print("""Usage: python3 enum4loser.py -d target.com -f name_of_folder
-
-        -d    Domain name.
-        -f    Name of the folder 
-Quick start: python3 enum4loser.py -d yeswehack.com -f ywhack""")
-            sys.exit()
-        elif opt in ("-d", "--domain"):
-            target = arg
-        elif opt in ("-f", "--folder"):
-            name_folder = arg
+    else:
+        if domain == None:
+            print('\033[91mIndicate a valid domain\033[0m \n')
+        if filename == None:
+            print('\033[91mIndicate a valid filename\033[0m \n')
+        else:
             creat_folder(home_path,name_folder,hunt_folder,target_folder)
             subdomain_enum(target,home_path,hunt_folder,target_folder,name_folder)
-        if opt in ("-s","--server"):
-            attacker_srv = arg
             scrap_url(home_path,hunt_folder,target_folder,name_folder,attacker_srv)
 
 if __name__ == "__main__":
-   main(sys.argv[1:])
+   main()
