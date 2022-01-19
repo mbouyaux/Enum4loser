4133dddb6d0f90650df0b3d2e83e2bd80103309f
Romain <74216407+spawnzii@users.noreply.github.com>
2022-01-19 14:08:44 +0100

    Update enum4loser.py

--- enum4loser.py
+++ enum4loser.py
@@ -8,6 +8,7 @@ from pathlib import Path
 from functions.creatfolder import *
 from functions.subenum import *
 from functions.urlwayback import *
+from functions.infoenum import *
 
 home_path = str(Path.home())
 urlsorting_parameter = ['=http','=https','=www','=/','=//']
@@ -17,29 +18,33 @@ target_folder = "target"
 
 @click.option('--domain','-d',help='target domain name')
 @click.option('--filename','-f',help='name of the output folder store at ~/Desktop/bugbouty/target/here')
-@click.option('--server','-s',default='https://www.google.com',help='You can add your web server to see the logs in live')
-@click.option('--nosub',default="False",help='This options enumerate juste the main domain and not the subdomain')
+@click.option('--server','-s',default='https://attacker.com/',help='You can add your web server to see the logs in live')
+@click.option('--nosub',default="false",help='This options enumerate juste the main domain and not the subdomain')
 
 def main(domain,filename,server,nosub):
     banner_one = pyfiglet.figlet_format("Enum4loser")
     print("\033[91m"+banner_one[0:232]+"\033[0m"+"\033[92m"+banner_one[232:300]+"\033[0m")
     print('Version 0.5')
     print('By SpawnZii made with \033[91m<3\033[0m \n')
-    name_folder = domain
-    target = filename
+    name_folder = filename
     attacker_srv = server
     nosub = str(nosub.lower())
-    if nosub == "true":
-        print("Under dev")
 
+    if domain == None:
+        print('\033[91mIndicate a valid domain\033[0m \n')
+        exit(0)
+    if filename == None:
+        print('\033[91mIndicate a valid filename\033[0m \n')
+        exit(0) 
     else:
-        if domain == None:
-            print('\033[91mIndicate a valid domain\033[0m \n')
-        if filename == None:
-            print('\033[91mIndicate a valid filename\033[0m \n')
+        if nosub == "true":
+            creat_folder(home_path,name_folder,hunt_folder,target_folder)
+            single_domain_url(domain,home_path,hunt_folder,target_folder,name_folder)
+            single_domain_filter(domain,home_path,hunt_folder,target_folder,name_folder)
+            openredirect(home_path,hunt_folder,target_folder,name_folder,attacker_srv)
         else:
             creat_folder(home_path,name_folder,hunt_folder,target_folder)
-            subdomain_enum(target,home_path,hunt_folder,target_folder,name_folder)
+            subdomain_enum(domain,home_path,hunt_folder,target_folder,name_folder)
             scrap_url(home_path,hunt_folder,target_folder,name_folder,attacker_srv)
 
 if __name__ == "__main__":
