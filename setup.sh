f5a8a7fe87616a56077d7d64b2447c094404de13
Romain <74216407+spawnzii@users.noreply.github.com>
2022-01-24 00:03:12 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ pip install -r requirements.txt
 git clone https://github.com/aboul3la/Sublist3r.git
 cd Sublist3r/ ;pip install -r requirements.txt;cp -r subbrute /usr/local/bin/ ; cp sublist3r.py /usr/local/bin/ ; cd ../ ; rm -rf Sublist3r/
 
-sudo apt install golang-go
-sudo apt install gccgo-go
+apt-get install -y golang-go
+apt-get install -y gccgo-go
 go get github.com/tomnomnom/waybackurls
 cp $HOME/go/bin/waybackurls /usr/local/bin/
