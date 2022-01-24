91448a6b18526bc19c11917a5724a7eef54c7ee9
Romain <74216407+spawnzii@users.noreply.github.com>
2022-01-24 09:45:08 +0100

    Update enum4loser.py

--- enum4loser.py
+++ enum4loser.py
@@ -9,12 +9,11 @@ from functions.creatfolder import *
 from functions.subenum import *
 from functions.urlwayback import *
 
-home_path = str(Path.home())
-urlsorting_parameter = ['=http','=https','=www','=/','=//']
-hunt_folder = "bugbounty"
-target_folder = "target"
-@click.command()
+HOME_PATH = str(Path.home())
+HUNT_FOLDER = "bugbounty"
+TARGET_FOLDER = "target"
 
+@click.command()
 @click.option('--domain','-d',help='target domain name')
 @click.option('--filename','-f',help='name of the output folder store at ~/Desktop/bugbouty/target/here')
 @click.option('--nosub',default="false",help='This options enumerate juste the main domain and not the subdomain, false by default')
@@ -30,20 +29,20 @@ def main(domain,filename,nosub):
 
     if domain is None:
         print('\033[91mIndicate a valid domain\033[0m \n')
-        exit(0)
+        sys.exit(0)
     if filename is None:
         print('\033[91mIndicate a valid filename\033[0m \n')
-        exit(0) 
+        sys.exit(0) 
     else:
         if nosub == "true":
-            create_folder(home_path,name_folder,hunt_folder,target_folder)
-            single_domain_url(domain,home_path,hunt_folder,target_folder,name_folder)
-            single_domain_filter(domain,home_path,hunt_folder,target_folder,name_folder)
-            openredirect(home_path,hunt_folder,target_folder,name_folder,attacker_srv)
+            create_folder(HOME_PATH,name_folder,HUNT_FOLDER,TARGET_FOLDER)
+            single_domain_url(domain,HOME_PATH,HUNT_FOLDER,TARGET_FOLDER,name_folder)
+            single_domain_filter(domain,HOME_PATH,HUNT_FOLDER,TARGET_FOLDER,name_folder)
+            openredirect(HOME_PATH,HUNT_FOLDER,TARGET_FOLDER,name_folder,attacker_srv)
         else:
-            create_folder(home_path,name_folder,hunt_folder,target_folder)
-            subdomain_enum(domain,home_path,hunt_folder,target_folder,name_folder)
-            scrap_url(home_path,hunt_folder,target_folder,name_folder,attacker_srv)
+            create_folder(HOME_PATH,name_folder,HUNT_FOLDER,TARGET_FOLDER)
+            subdomain_enum(domain,HOME_PATH,HUNT_FOLDER,TARGET_FOLDER,name_folder)
+            scrap_url(HOME_PATH,HUNT_FOLDER,TARGET_FOLDER,name_folder,attacker_srv)
 
 if __name__ == "__main__":
     main()
